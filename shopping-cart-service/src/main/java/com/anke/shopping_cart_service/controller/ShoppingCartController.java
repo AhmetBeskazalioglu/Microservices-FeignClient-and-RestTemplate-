184fea7392a056be31a7380bb40ae7ecfbf58d82
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-07-17 19:03:15 +0300

    shopping-cart-service is builded

---
+++ shopping-cart-service/src/main/java/com/anke/shopping_cart_service/controller/ShoppingCartController.java
@@ -0,0 +1,35 @@
+package com.anke.shopping_cart_service.controller;
+
+import com.anke.shopping_cart_service.entity.ShoppingCart;
+import com.anke.shopping_cart_service.service.ShoppingCartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/shopping-cart")
+public class ShoppingCartController {
+
+    @Autowired
+    ShoppingCartService shoppingCartService;
+
+    @PostMapping
+    public ResponseEntity<ShoppingCart> create(@RequestParam("name") String name) {
+        return shoppingCartService.create(name);
+    }
+
+    @PostMapping("{id}")
+    public ResponseEntity<ShoppingCart> addProducts(
+            @PathVariable("id") Long shoppingCartId, @RequestBody List<Product> products) {
+        return shoppingCartService.addProducts(shoppingCartId, products);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Map<String, String>> getShoppingCartPrice(
+            @PathVariable("id") Long shoppingCartId) {
+        return shoppingCartService.getShoppingCartPrice(shoppingCartId);
+    }
+}
---
+++ shopping-cart-service/src/main/java/com/anke/shopping_cart_service/entity/Product.java
@@ -0,0 +1,45 @@
+package com.anke.shopping_cart_service.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.Set;
+
+@Entity
+public class Product {
+
+	@Id
+	private Long id;
+
+	private String name;
+
+	@JsonIgnore // Prevent infinite recursion
+	@ManyToMany(mappedBy = "products")
+	private Set<ShoppingCart> shoppingCarts;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<ShoppingCart> getShoppingCarts() {
+		return shoppingCarts;
+	}
+
+	public void setShoppingCarts(Set<ShoppingCart> shoppingCarts) {
+		this.shoppingCarts = shoppingCarts;
+	}
+}
